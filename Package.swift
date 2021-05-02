0f36765d6b244226efb07ee559749edd4d59eaf2
Alex <alex.buzov@icloud.com>
2021-05-02 14:21:53 -0700

    tvOS Support

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "NetworkExtensions",
     platforms: [
-        .iOS(.v12), .macOS(.v10_14)
+        .iOS(.v12), .macOS(.v11), .tvOS(.v14)
     ],
     products: [
         .library(name: "NetworkExtensions", type: .static, targets: ["NetworkExtensions"])
